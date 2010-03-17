f2cc0895d64a25ec636c8f1f5b3a05475de0a5e7
Hans-Christian Esperer <hc@hcesperer.org>
2010-03-17 23:13:41 +0100

    Extended test.py

--- test.py
+++ test.py
@@ -1,8 +1,17 @@
 import avrnetio
+import time
 
 def doit():
     x = avrnetio.AVRNetIO(('192.168.0.90', 50290))
-    x.getPort(1)
+    for i in range(1, 5):
+        print "Status of port %d: %d" % (i, x.getPort(i))
+    for i in range(1, 5):
+        print "A/D port %d: %d" % (i, x.getADC(i))
+    print "Setting ports:"
+    for i in range(0, 16):
+        x.setPort((i % 8) + 1, i < 8)
+        print "Set port %d to %s." % ((i % 8) + 1, i < 8)
+        time.sleep(1)
 
 if __name__ == '__main__':
     doit()
